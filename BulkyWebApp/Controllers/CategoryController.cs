2d26a9e0a6f0d79283716e30f59ea3f5556390b9
shubh <shubh@SHUBHAM>
2023-11-23 16:48:22 +0530

    Add project files.

---
+++ BulkyWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebApp.Data;
+
+public class ApplicationDbContext : DbContext
+
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Category> Categories { get; set; }
+}
